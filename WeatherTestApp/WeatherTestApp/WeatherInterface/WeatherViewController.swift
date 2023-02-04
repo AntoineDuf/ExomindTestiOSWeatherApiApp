f09ba6d876a135b73badceefa7fabc39fdd877e4
Antoine Dufayet <antoine.dufayet@gmail.com>
2023-02-04 16:34:46 +0100

    add of WeatherInterface files and add objects in weather storyboard

---
+++ WeatherTestApp/WeatherTestApp/WeatherInterface/WeatherViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherViewController.swift
+//  WeatherTestApp
+//
+//  Created by Antoine Dufayet on 04/02/2023.
+//
+
+import UIKit
+
+final class WeatherViewController: UIViewController {
+    
+    private let viewModel = WeatherViewModel()
+}
---
+++ WeatherTestApp/WeatherTestApp/WeatherInterface/WeatherViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherViewModel.swift
+//  WeatherTestApp
+//
+//  Created by Antoine Dufayet on 04/02/2023.
+//
+
+import Foundation
+
+final class WeatherViewModel {
+    
+}
